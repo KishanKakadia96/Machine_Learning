753e304f55a270139665b9e1411996bb7f1196cb
KishanKakadia96 <kishankakadia96@gmail.com>
2022-08-31 23:16:11 +0200

    Testing of Logistic regression implemented

--- Logistic_regression.py
+++ Logistic_regression.py
@@ -36,3 +36,26 @@ class LogisticRegression:
 
     def _sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
+
+# Testing
+if __name__ == "__main__":
+    # Imports
+    from sklearn.model_selection import train_test_split
+    from sklearn import datasets
+
+    def accuracy(y_true, y_pred):
+        accuracy = np.sum(y_true == y_pred) / len(y_true)
+        return accuracy
+
+    bc = datasets.load_breast_cancer()
+    X, y = bc.data, bc.target
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=1234
+    )
+
+    regressor = LogisticRegression(learning_rate=0.0001, n_iters=1000)
+    regressor.fit(X_train, y_train)
+    predictions = regressor.predict(X_test)
+
+    print("LR classification accuracy:", accuracy(y_test, predictions))
