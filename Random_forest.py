f388288dcb0e9642be80d005f7680736dff55739
KishanKakadia96 <kishankakadia96@gmail.com>
2022-09-01 18:42:20 +0200

    load dataset

---
+++ Random_forest.py
@@ -0,0 +1,33 @@
+from collections import Counter
+
+import numpy as np
+
+from .Decision_tree import DecisionTree
+
+def bootstrap_sample(X, y):
+    n_samples = X.shape[0]
+    idxs = np.random.choice(n_samples, n_samples, replace=True)
+    return X[idxs], y[idxs]
+
+def most_common_label(y):
+    counter = Counter(y)
+    most_common = counter.most_common(1)[0][0]
+    return most_common
+
+# Testing
+if __name__ == "__main__":
+    # Imports
+    from sklearn import datasets
+    from sklearn.model_selection import train_test_split
+
+    def accuracy(y_true, y_pred):
+        accuracy = np.sum(y_true == y_pred) / len(y_true)
+        return accuracy
+
+    data = datasets.load_breast_cancer()
+    X = data.data
+    y = data.target
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=1234
+    )
